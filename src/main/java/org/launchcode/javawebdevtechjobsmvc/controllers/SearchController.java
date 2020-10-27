3b933643b15c5e0013f51127aa2e392370c46c61
Cas <testing@owners-MacBook-Pro-2.local>
2020-10-26 20:06:26 -0500

    Still trying to get search/results to render

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -5,6 +5,8 @@ import org.launchcode.javawebdevtechjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.ArrayList;
 
@@ -24,5 +26,21 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("/search/results") //Step 1
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) { //Step 2,3,4
+        ArrayList<Job> jobs;
+
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){ //Step 5
+            model.addAttribute("title", "All Jobs");
+            jobs = JobData.findAll();
+
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);//Step 6 Passing jobs into the search.html via model parameter
+        model.addAttribute("columns", columnChoices); // Step 7passing ListController.columnChoices
+
+        return "/search/results";
+    }
 
 }
